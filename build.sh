9b34ee29afbc67157977f37f1bcc07959ba5b5c0
William Jackson <william@subtlecoolness.com>
2020-07-14 16:57:16 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -e
 
 # install os dependencies
-# apk add --no-cache gcc musl-dev
+apk add --no-cache g++ gcc musl-dev
 
 # build
-pip wheel --no-deps --requirement requirements.txt
+pip wheel --no-deps --extra-index-url https://alpine-wheels.github.io/index --requirement requirements.txt
