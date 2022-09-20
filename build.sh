34cf763664b4177395dc82539940f8c9bbf593fb
William Jackson <565174+williamjacksn@users.noreply.github.com>
2022-09-20 10:34:23 -0500

    Upgrade to latest version of pip

--- build.sh
+++ build.sh
@@ -5,5 +5,7 @@ set -e
 # install os dependencies
 apk add --no-cache g++ gcc musl-dev
 
+pip install --upgrade pip
+
 # build
 pip wheel --no-deps --extra-index-url https://alpine-wheels.github.io/index --requirement requirements.txt
